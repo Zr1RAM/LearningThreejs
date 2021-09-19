e9a243e08731483afb4558b91e7fd6193f655675
Zr1RAM <sriram.varadarajulu@gmail.com>
2021-09-19 23:27:58 +0530

    Trying to make scene into a separate class

---
+++ src/js/app/Scenes/MainScene.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+
+
+
+// Local imports -
+// Components
+import Camera from './components/camera';
+import Light from './components/light';
+import Controls from './components/controls';
+import Geometry from './components/geometry';
